4980a8e155210bac06623de22dd62e6a2e018ff6
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-11-23 00:19:49 +0530

    Lecture - 5 Completed

---
+++ Frontend - Chai Aur Code/Javascript/src/01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); //we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Meet");
+
+let name = "Meet";
+let age = 37;
+let isLoggedIn = false;
+
+// ---------------------------------------------------
+// ---: Primitive Datatypes :---
+// ---------------------------------------------------
+
+// number => range of "number" datatype is approx -> 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value
+// undefined => currently value is not assigned
+// symbol => unique
+
+console.log(typeof "Meet");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
